5e3d82595e9037fcbb9d260d9063588ebc973039
Harsh Krishnani <harshkrishnani2409@gmail.com>
2026-06-01 05:38:40 +0530

    feat: add support for custom delimiters in addition

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,6 +5,11 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
+    if numbers.start_with?('//')
+      delimiter, numbers = numbers[2..].split("\n", 2)
+      return numbers.split(delimiter).sum(&:to_i)
+    end
+
     numbers.split(/[,\n]/).sum(&:to_i)
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -22,5 +22,9 @@ RSpec.describe StringCalculator do
     it 'handles newlines as delimiters' do
       expect(calculator.add("1\n2,3")).to eq(6)
     end
+
+    it 'returns sum of numbers with custom delimiter' do
+      expect(calculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
